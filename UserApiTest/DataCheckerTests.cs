7286ce831cf7b5583ade4523e24d52027788fefe
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2024-01-31 00:38:34 +0300

    Добавлены unit тесты

---
+++ UserApiTest/DataCheckerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+using UserAPI.Infrastructure;
+
+namespace UserApiTest
+{
+    public class DataCheckerTests
+    {
+        [Fact]
+        public void IsEmailTrueResultTest()
+        {
+            // Arrange
+            string email = "user@yandex.ru";
+
+            // Assert
+            Assert.True(DataChecker.IsEmail(email));
+        }
+
+        [Fact]
+        public void IsEmailFalseResultTest()
+        {
+            // Arrange
+            string email = "user#yandex.ru";
+
+            // Assert
+            Assert.False(DataChecker.IsEmail(email));
+        }
+        [Fact]
+        public void CheckPasswordTrueResultTest()
+        {
+            // Arrange
+            string password = "Hello1111";
+
+            // Assert
+            Assert.True(DataChecker.CheckPassword(password));
+        }
+
+        [Fact]
+        public void CheckPasswordSizeFalseResultTest()
+        {
+            // Arrange
+            string password = "Hello1";
+
+            // Assert
+            Assert.False(DataChecker.CheckPassword(password));
+        }
+        [Fact]
+        public void CheckPasswordWithOutUpperFalseResultTest()
+        {
+            // Arrange
+            string password = "hello1111";
+
+            // Assert
+            Assert.False(DataChecker.CheckPassword(password));
+        }
+        [Fact]
+        public void CheckPasswordWithOutNumberFalseResultTest()
+        {
+            // Arrange
+            string password = "Helloooooo";
+
+            // Assert
+            Assert.False(DataChecker.CheckPassword(password));
+        }
+    }
+}
